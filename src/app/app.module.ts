6ae98af1ba7bae5b2c53c00bda52f3ef1cb61b20
KRLokesh007 <38097267+KRLokesh007@users.noreply.github.com>
2022-11-05 10:30:15 +0530

    Lokesh https://github.com/KRLokesh007/amazon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { StudentComponent } from './student/student.component';
+import { CollegetComponent } from './colleget/colleget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    StudentComponent,
+    CollegetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/colleget/colleget.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-colleget',
+  template: `
+    <p>
+      colleget works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CollegetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
